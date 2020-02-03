2d1af76774e43d40cd367ed93b1d04b072afd4bc
Devanshi Pratiher <devanshi.pratiher@zineone.com>
2020-02-02 23:37:41 -0500

    Finished coding the Flix Movie App

---
+++ flix movie app/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix movie app
+//
+//  Created by Devanshi Pratiher on 2/2/20.
+//  Copyright © 2020 Devanshi Pratiher. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
